7fb08bd8d82e03e73217bdd23b8eab501a916ec6
NathanielSolivio <164489939+NathanielSolivio@users.noreply.github.com>
2024-03-22 21:38:18 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
 const paper4 = document.querySelector("#p4");
+const paper5 = document.querySelector("#p5");
+const paper6 = document.querySelector("#p6");
+const paper7 = document.querySelector("#p7");
+const paper8 = document.querySelector("#p8");
+const paper9 = document.querySelector("#p9");
 
 // Event Listener
 prevBtn.addEventListener("click", goPrevPage);
@@ -14,7 +19,7 @@ nextBtn.addEventListener("click", goNextPage);
 
 // Business Logic
 let currentLocation = 1;
-let numOfPapers = 4;
+let numOfPapers = 9;
 let maxLocation = numOfPapers + 1;
 
 function openBrochure() {
@@ -53,8 +58,28 @@ function goNextPage() {
             case 4:
                 paper4.classList.add("flipped");
                 paper4.style.zIndex = 4;
+                break;   
+            case 5:
+                paper5.classList.add("flipped");
+                paper5.style.zIndex = 5;
+                break;   
+            case 6:
+                paper6.classList.add("flipped");
+                paper6.style.zIndex = 6;
+                break;   
+            case 7:
+                paper7.classList.add("flipped");
+                paper7.style.zIndex = 7;
+                break;    
+            case 8:
+                paper8.classList.add("flipped");
+                paper8.style.zIndex = 8;
+                break;    
+            case 9:
+                paper9.classList.add("flipped");
+                paper9.style.zIndex = 9;
                 closeBrochure(false);
-                break;            
+                break;         
             default:
                 throw new Error("unkown state");
         }
@@ -68,20 +93,40 @@ function goPrevPage() {
             case 2:
                 closeBrochure(true);
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 4;
+                paper1.style.zIndex = 9;
                 break;
             case 3:
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 3;
+                paper2.style.zIndex = 8;
                 break;
             case 4:
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 2;
+                paper3.style.zIndex = 7;
                 break;
             case 5:
-                openBrochure();
                 paper4.classList.remove("flipped");
-                paper4.style.zIndex = 1;
+                paper4.style.zIndex = 6;
+                break;
+            case 6:
+                paper5.classList.remove("flipped");
+                paper5.style.zIndex = 5;
+                break;
+            case 7:
+                paper6.classList.remove("flipped");
+                paper6.style.zIndex = 4;
+                break;
+            case 8:
+                paper7.classList.remove("flipped");
+                paper7.style.zIndex = 3;
+                break;
+            case 9:
+                paper8.classList.remove("flipped");
+                paper8.style.zIndex = 2;
+                break;
+            case 10:
+                openBrochure();
+                paper9.classList.remove("flipped");
+                paper9.style.zIndex = 1;
                 break;
             default:
                 throw new Error("unkown state");
